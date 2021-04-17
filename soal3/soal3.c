95434bfae1cb9dfe426370ba722bc6dcd87d8fef
Ramadhan Arif H <dani.ramadhan7@gmail.com>
2021-04-17 21:20:58 +0700

    Update soal3.c
    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,14 +1,17 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
-#include <syslog.h>
-#include <string.h>
 #include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/prctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <syslog.h>
 #include <time.h>
+#include <unistd.h>
+#include <wait.h>
 
 int main(int argc, char const *argv[])
 {
@@ -26,7 +29,7 @@ int main(int argc, char const *argv[])
 
     if (child_id == 0)
     {
-        
+
     }
     else
     {
@@ -38,7 +41,7 @@ int main(int argc, char const *argv[])
             time(&rawtime);
             timeinfo = localtime(&rawtime);
             strftime(timestamp, 50, "%Y-%m-%d_%X", timeinfo);
-            printf("%s\n", timestamp);
+            // printf("%s\n", timestamp);
 
             // Create a directory with a timestamped name
             int check = mkdir(timestamp, 0777);
