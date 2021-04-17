e113b4bb8bc878c0e4f4a46761f6b684c0333021
Ramadhan Arif H <dani.ramadhan7@gmail.com>
2021-04-17 20:50:59 +0700

    Update soal3.c
    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -8,33 +8,45 @@
 #include <syslog.h>
 #include <string.h>
 #include <dirent.h>
-#include <string.h>
 #include <time.h>
 
 int main(int argc, char const *argv[])
 {
     time_t rawtime;
-    struct tm * timeinfo;
-    char timestamp[50] = { 0 };
+    struct tm *timeinfo;
+    char timestamp[50] = {0};
+    pid_t child_id;
 
-    while (1)
+    child_id = fork();
+
+    if (child_id < 0)
     {
-        // Get timestamped name
-        time(&rawtime);
-        timeinfo = localtime(&rawtime);
-
-        strftime(timestamp, 50, "%Y-%m-%d_%X", timeinfo);
-        printf("%s\n", timestamp);
-        // END
-
-        // Create a directory with a timestamped name
-        int check = mkdir(timestamp, 0777);
-        // END
-
-        // Every 40 seconds
-        sleep(40);
-        // END
+        exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
     }
+
+    if (child_id == 0)
+    {
+        
+    }
+    else
+    {
+        // this is parent
+
+        while (1)
+        {
+            // Get timestamped name
+            time(&rawtime);
+            timeinfo = localtime(&rawtime);
+            strftime(timestamp, 50, "%Y-%m-%d_%X", timeinfo);
+            printf("%s\n", timestamp);
+
+            // Create a directory with a timestamped name
+            int check = mkdir(timestamp, 0777);
+
+            // Every 40 seconds
+            sleep(40);
+        }
+    }    
 
     return 0;
 }
